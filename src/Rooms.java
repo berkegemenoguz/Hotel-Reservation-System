8ac3bb789cdd92277fc36a835c9717fe55cee42e
berkegemenoguz <berkegemenoguz@hotmail.com>
2025-12-11 21:32:47 +0300

    Rooms Class Added.

---
+++ src/Rooms.java
@@ -0,0 +1,49 @@
+
+public abstract class Rooms implements Bookings{
+
+    private int RoomNumber;
+    private double NormalPrice;
+    private boolean isOccupied;
+
+    public Rooms(int RoomNumber, double NormalPrice){
+
+        this.NormalPrice = NormalPrice;
+        this.RoomNumber = RoomNumber;
+        this.isOccupied = false;
+
+        }
+
+    public int getRoomNumber() {
+        return RoomNumber;
+    }
+
+    public void setRoomNumber(int roomNumber) {
+        RoomNumber = roomNumber;
+    }
+
+    public double getNormalPrice() {
+        return NormalPrice;
+    }
+
+    public void setNormalPrice(double normalPrice) {
+        NormalPrice = normalPrice;
+    }
+
+    public boolean isOccupied() {
+        return isOccupied;
+    }
+
+    public void setOccupied(boolean occupied) {
+        isOccupied = occupied;
+    }
+
+    @Override
+    public abstract double CalculatePrice(int Days);
+
+    @Override
+    public abstract String getRoomDetails();
+
+
+
+
+}
