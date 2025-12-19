bdc3a79c7f02ea7694564a127011abc6ac212997
berkegemenoguz <berkegemenoguz@hotmail.com>
2025-12-19 12:18:21 +0300

    Added prd function.

--- src/Reservation.java
+++ src/Reservation.java
@@ -13,14 +13,28 @@ public class Reservation {
             this.Client = Client;
             this.Room = Room;
             this.StayDays = stayDays;
-
             this.TotalPrice = Room.CalculatePrice(stayDays);
-
 
             Room.setOccupied(true);
 
             System.out.println("Reservation Created.");
         }
     }
+    public void prd() {
 
+        if (Client != null && Room != null) {
+            System.out.println("\n------------------------Reception Receipt");
+            System.out.println("Customer Info: ");
+            System.out.println("Full Name: "+  Client.getName()  + " " +  Client.getSurname());
+            System.out.println("Email: " + Client.getEmail());
+            System.out.println("Phone Number: " + Client.getPhoneNumber());
+            System.out.println("ID: " + Client.getId());
+            System.out.println("----------------------------------------");
+            System.out.println("Oda Bilgisi   : " + Room.getRoomDetails());
+            System.out.println("Konaklama     : " + StayDays + " Gün");
+            System.out.println("TOPLAM TUTAR  : " + TotalPrice + " TL");
+            System.out.println("----------------------------------------\n");
+
+        }
+    }
 }
