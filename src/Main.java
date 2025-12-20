905023f68c38404b8fe41a3efa5147e65401e12f
berkegemenoguz <berkegemenoguz@hotmail.com>
2025-12-20 21:24:06 +0300

    Added Main Class.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("\n---Hotel System is Launching---\n");
+
+
+        Customer musteri1 = new Customer(1, "Ahmad", "Gurkan", "0555-111-2233", "ahmet@ornek.com");
+        Customer musteri2 = new Customer(2, "Zeynep", "Kaya", "0555-444-5566", "zeynep@ornek.com");
+
+
+        Rooms oda101 = new NormalRoom(101, 1000.0);
+        Rooms oda201 = new SuiteRoom(201, 2500.0, 500.0);
+
+
+        Reservation r1 = new Reservation(musteri1, oda201, 3);
+        r1.prd();
+
+
+        System.out.println("\n------New Reservation-------\n");
+        Reservation r3 = new Reservation(musteri2, oda101, 5);
+        r3.prd();
     }
 }
--- src/Reservation.java
+++ src/Reservation.java
@@ -17,24 +17,26 @@ public class Reservation {
 
             Room.setOccupied(true);
 
-            System.out.println("Reservation Created.");
+            System.out.println("\nReservation Created.");
         }
     }
+
     public void prd() {
 
         if (Client != null && Room != null) {
-            System.out.println("\n------------------------Reception Receipt");
+            System.out.println("\n-----------Reception Receipt------------");
             System.out.println("Customer Info: ");
-            System.out.println("Full Name: "+  Client.getName()  + " " +  Client.getSurname());
+            System.out.println("Full Name: " + Client.getName() + " " + Client.getSurname());
             System.out.println("Email: " + Client.getEmail());
             System.out.println("Phone Number: " + Client.getPhoneNumber());
             System.out.println("ID: " + Client.getId());
             System.out.println("----------------------------------------");
-            System.out.println("Oda Bilgisi   : " + Room.getRoomDetails());
-            System.out.println("Konaklama     : " + StayDays + " Gün");
-            System.out.println("TOPLAM TUTAR  : " + TotalPrice + " TL");
+            System.out.println("Room Info     : " + Room.getRoomDetails());
+            System.out.println("Days to stay  : " + StayDays );
+            System.out.println("Total Amount  : " + TotalPrice + " TL");
             System.out.println("----------------------------------------\n");
 
         }
     }
+
 }
