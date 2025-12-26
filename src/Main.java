51aab0240e45fbdf404aed2b620bb52a04bec584
berkegemenoguz <berkegemenoguz@hotmail.com>
2025-12-26 18:00:50 +0300

    The Main class has been modified to accommodate the newly added RoomService class.

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,25 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("\n---Hotel System is Launching---");
 
-        System.out.println("\n---Hotel System is Launching---\n");
-
-
-        Customer musteri1 = new Customer(1, "Ahmad", "Gurkan", "0555-111-2233", "ahmet@ornek.com");
-        Customer musteri2 = new Customer(2, "Zeynep", "Kaya", "0555-444-5566", "zeynep@ornek.com");
-
-
-        Rooms oda101 = new NormalRoom(101, 1000.0);
+        Customer musteri1 = new Customer(1, "Ahmad", "Gurkan", "111-222-333", "ahmet@ornek.com");
         Rooms oda201 = new SuiteRoom(201, 3000.0);
 
 
+
+
+        RoomService gym = new RoomService("Gym Membership", 500);
+        RoomService valet = new RoomService("Vale Service", 200);
+        RoomService extraBed = new RoomService("Extra Bed", 750);
+        RoomService transfer = new RoomService("Airport Transportation", 1200);
+
         Reservation r1 = new Reservation(musteri1, oda201, 3);
+
+        r1.addService(gym);
+        r1.addService(valet);
+        r1.addService(transfer);
+        r1.addService(extraBed);
+
         r1.prd();
-
-
-        System.out.println("\n------New Reservation-------\n");
-        Reservation r3 = new Reservation(musteri2, oda101, 5);
-        r3.prd();
     }
 }
