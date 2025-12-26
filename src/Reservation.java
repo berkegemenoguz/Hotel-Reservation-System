859c61eea5ba8ea9af7c2cdf032b31a63ab5922c
berkegemenoguz <berkegemenoguz@hotmail.com>
2025-12-26 08:43:34 +0300

    Updated Reservation Class for RoomService future. Added addService function.

--- src/Reservation.java
+++ src/Reservation.java
@@ -1,28 +1,43 @@
+import java.util.ArrayList;
+
 public class Reservation {
     private Customer Client;
     private Rooms Room;
     private int StayDays;
     private double TotalPrice;
 
+
+    private ArrayList<RoomService> extraServices;
+
     public Reservation(Customer Client, Rooms Room, int stayDays) {
-
         if (Room.isOccupied()) {
-            System.out.println("Warning: " + "Room" + " " + Room.getRoomNumber() + " is full.");
+            System.out.println("Warning: Room " + Room.getRoomNumber() + " is full.");
         } else {
-
             this.Client = Client;
             this.Room = Room;
             this.StayDays = stayDays;
+
+
             this.TotalPrice = Room.CalculatePrice(stayDays);
 
-            Room.setOccupied(true);
 
-            System.out.println("\nReservation Created.");
+            this.extraServices = new ArrayList<>();
+
+            Room.setOccupied(true);
+            System.out.println("\nReservation Created for " + Client.getName());
+        }
+    }
+
+
+    public void addService(RoomService service) {
+        if (this.Client != null) {
+            this.extraServices.add(service);
+            this.TotalPrice += service.getServicePrice();
+            System.out.println("Service Added: " + service.getServiceName());
         }
     }
 
     public void prd() {
-
         if (Client != null && Room != null) {
             System.out.println("\n-----------Reception Receipt------------");
             System.out.println("Customer Info: ");
@@ -31,12 +46,20 @@ public class Reservation {
             System.out.println("Phone Number: " + Client.getPhoneNumber());
             System.out.println("ID: " + Client.getId());
             System.out.println("----------------------------------------");
-            System.out.println("Room Info     : " + Room.getRoomDetails());
-            System.out.println("Days to stay  : " + StayDays );
-            System.out.println("Total Amount  : " + TotalPrice + " TL");
-            System.out.println("----------------------------------------\n");
+            System.out.println("Room Info      : " + Room.getRoomDetails());
+            System.out.println("Days to stay   : " + StayDays );
+            System.out.println("Room Price     : " + Room.CalculatePrice(StayDays) + " TL");
 
+            if (!extraServices.isEmpty()) {
+                System.out.println("--- Extras ---");
+                for (RoomService s : extraServices) {
+                    System.out.println(" * " + s.getServiceName() + " : " + s.getServicePrice() + " TL");
+                }
+            }
+
+            System.out.println("----------------------------------------");
+            System.out.println("Total Amount   : " + TotalPrice + " TL");
+            System.out.println("----------------------------------------\n");
         }
     }
-
 }
