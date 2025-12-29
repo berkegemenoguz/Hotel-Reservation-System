a778aa90d21303850f9521612b14470dee60cb2e
berkegemenoguz <berkegemenoguz@hotmail.com>
2025-12-29 14:31:02 +0300

    Crated JUnit Test class and set up the infrastructure

--- src/HotelReservationTest.java
+++ src/HotelReservationTest.java
@@ -1,2 +1,19 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Hotel-Reservation-System")
 public class HotelReservationTest {
+
+    private Customer customer;
+    private NormalRoom normalRoom;
+    private SuiteRoom suiteRoom;
+
+    @BeforeEach
+    void setUp() {
+        customer = new Customer(1, "Bilal", "Hancı", "bilalhanci@gmail.com", "321321");
+        normalRoom = new NormalRoom(101, 500.0);
+        suiteRoom = new SuiteRoom(201, 1000.0);
+    }
 }
