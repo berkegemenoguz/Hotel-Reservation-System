026c0f9d40403866258e9791ad4007577c94e7f8
berkegemenoguz <berkegemenoguz@hotmail.com>
2025-12-30 16:47:20 +0300

    Added new JUnit Tests for price calculation and customer properties

--- src/HotelReservationTest.java
+++ src/HotelReservationTest.java
@@ -16,4 +16,24 @@ public class HotelReservationTest {
         normalRoom = new NormalRoom(101, 500.0);
         suiteRoom = new SuiteRoom(201, 1000.0);
     }
+
+    @Test
+    @DisplayName("Customer properties are set correctly.")
+    void testCustomerCreation() {
+        assertEquals("Bilal", customer.getName(), "Customer name should be Bilal.");
+        assertEquals("bilalhanci@gmail.com", customer.getEmail(), "Customer email should match.");
+    }
+
+    @Test
+    @DisplayName("Price calculation works for different room types")
+    void testRoomPriceCalculation() {
+        assertEquals(2500.0, normalRoom.CalculatePrice(5), "Normal room price for 5 days should be 2500");
+        assertEquals(3000.0, suiteRoom.CalculatePrice(3), "Suite room price for 3 days should be 3000");
+    }
+
+
+
+
+
+
 }
