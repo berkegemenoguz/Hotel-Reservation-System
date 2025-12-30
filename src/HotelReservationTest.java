7bf747436765d8b49a6024e2bd8a4b69f3810c13
berkegemenoguz <berkegemenoguz@hotmail.com>
2025-12-30 17:53:21 +0300

    Added new JUnit Test for addition of the services into reservation total.

--- src/HotelReservationTest.java
+++ src/HotelReservationTest.java
@@ -7,14 +7,14 @@ import static org.junit.jupiter.api.Assertions.*;
 public class HotelReservationTest {
 
     private Customer customer;
-    private NormalRoom normalRoom;
-    private SuiteRoom suiteRoom;
+    private NormalRoom NormalRoom;
+    private SuiteRoom SuiteRoom;
 
     @BeforeEach
     void setUp() {
         customer = new Customer(1, "Bilal", "Hancı", "bilalhanci@gmail.com", "321321");
-        normalRoom = new NormalRoom(101, 500.0);
-        suiteRoom = new SuiteRoom(201, 1000.0);
+        NormalRoom = new NormalRoom(101, 500.0);
+        SuiteRoom = new SuiteRoom(201, 1000.0);
     }
 
     @Test
@@ -27,27 +27,34 @@ public class HotelReservationTest {
     @Test
     @DisplayName("Price calculation works for different room types.")
     void testRoomPriceCalculation() {
-        assertEquals(2500.0, normalRoom.CalculatePrice(5), "Normal room price for 5 days should be 2500");
-        assertEquals(3000.0, suiteRoom.CalculatePrice(3), "Suite room price for 3 days should be 3000");
+        assertEquals(2500.0, NormalRoom.CalculatePrice(5), "Normal room price for 5 days should be 2500");
+        assertEquals(3000.0, SuiteRoom.CalculatePrice(3), "Suite room price for 3 days should be 3000");
     }
 
     @Test
     @DisplayName("Room details returns the correct format.")
     void testRoomDetails() {
-        assertEquals("Normal Room ==> Room No: 101", normalRoom.getRoomDetails(),
+        assertEquals("Normal Room ==> Room No: 101", NormalRoom.getRoomDetails(),
                 "Normal room details format is incorrect");
-        assertEquals("Suite Room ==> Room No: 201", suiteRoom.getRoomDetails(),
+        assertEquals("Suite Room ==> Room No: 201", SuiteRoom.getRoomDetails(),
                 "Suite room details format is incorrect");
     }
 
     @Test
     @DisplayName("Reservation action creates and marks the room as occupied.")
     void testReservationCreation() {
-        Reservation reservation = new Reservation(customer, normalRoom, 3);
+        Reservation reservation = new Reservation(customer, NormalRoom, 3);
         assertEquals(1500.0, reservation.getTotalPrice(), "Reservation total should be 1500.");
-        assertTrue(normalRoom.isOccupied(), "Room should be marked as occupied after reservation.");
+        assertTrue(NormalRoom.isOccupied(), "Room should be marked as occupied after reservation.");
     }
 
-
+    @Test
+    @DisplayName("Service price is successfully added to reservation total.")
+    void testAddServiceToReservation() {
+        Reservation reservation = new Reservation(customer, NormalRoom, 2);
+        RoomService breakfast = new RoomService("Breakfast", 100);
+        reservation.addService(breakfast);
+        assertEquals(1100.0, reservation.getTotalPrice(), "Total should include service price.");
+    }
 
 }
