e7b9b9c59f6d55ccd1041ac71b57c9a5eaa4c8a8
berkegemenoguz <berkegemenoguz@hotmail.com>
2026-01-03 14:25:11 +0300

    JUnit Tests for customer info.

---
+++ src/HotelReservationTest.java
@@ -0,0 +1,42 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
+public class HotelReservationTest {
+
+    private Customer testCustomer;
+    private NormalRoom testNormalRoom;
+    private SuiteRoom testSuiteRoom;
+
+    @BeforeEach
+    void setUp() {
+        testCustomer = new Customer(1, "Caner", "Asilioglu", "caner@gmail.com", "321312312");
+        testNormalRoom = new NormalRoom(101);
+        testSuiteRoom = new SuiteRoom(201);
+    }
+
+    //-----------------Customer Tests-----------------
+
+    @Test
+    void testCustomerCreation() {
+        assertEquals(1, testCustomer.getId());
+        assertEquals("Caner", testCustomer.getName());
+        assertEquals("Asilioglu", testCustomer.getSurname());
+        assertEquals("caner@gmail.com", testCustomer.getEmail());
+        assertEquals("321312312", testCustomer.getPhoneNumber());
+    }
+
+    @Test
+    void testCustomerSetters() {
+        testCustomer.setName("davut");
+        testCustomer.setSurname("cuci");
+        testCustomer.setEmail("davut@gmail.com");
+
+        assertEquals("davut", testCustomer.getName());
+        assertEquals("cuci", testCustomer.getSurname());
+        assertEquals("davut@gmail.com", testCustomer.getEmail());
+    }
+
+}
